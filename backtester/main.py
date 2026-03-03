500f19ebf734a7c5b94678bb7b05eb82c55830fc
cole-koryto <colekoryto17@gmail.com>
2026-03-03 13:10:30 -0600

    Backtesting outline

---
+++ backtester/main.py
@@ -0,0 +1,20 @@
+from backtester_engine import Backtester
+from performance_output import plot_portfolio_history, print_portfolio_stats
+
+def main():
+    pass
+    # Get signals data
+    # signals_df = get_signals()
+
+    # Run backtesting simulation given parameters
+    # backtester = Backtester(signals_df=signals_df)
+    # backtester.run_backtest()
+
+    # Display results
+    # backtest_trade_log, backtest_value_log = backtester.get_backtest_results()
+    # print_portfolio_stats(backtest_value_log)
+    # plot_portfolio_history(backtest_value_log)
+
+
+if __name__ == "__main__":
+    main()
---
+++ backtester/performance_output.py
@@ -0,0 +1,10 @@
+
+
+def print_portfolio_stats():
+    # sharpe, skew, kurt, etc...
+    pass
+
+
+def plot_portfolio_history():
+    # plot graph of portfolio value over time (optionally with trades displayed)
+    pass
