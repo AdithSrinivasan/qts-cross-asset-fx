3167a8dc6f556a949203489f3bbd023ed459b6a8
Jonathan Kim <112917210+Jonathan4Kim@users.noreply.github.com>
2026-03-05 14:32:04 -0600

    literally nothing

--- backtester/backtester_engine.py
+++ backtester/backtester_engine.py
@@ -1,13 +1,28 @@
 import pandas as pd
+from pandas import DataFrame
+import logging
+from typing import Optional, float, int
 
 class Backtester:
-    def __init__(self, signals_df,
-                 trading_cost=0.01,
-                 starting_cash=100000,
-                 q_trading_thres=0.2,
-                 fx_look_ahead=1,
-                 leverage_multiplier=5.0):
-        self.signals_df = signals_df.copy()
+    def __init__(self, 
+                 # new dataframes generated from regression passed
+                 train_predictions: Optional[DataFrame], 
+                 test_predictions: Optional[DataFrame], 
+                 entry_thresholds: Optional[DataFrame],
+                 exit_thresholds: Optional[DataFrame],
+                 # changed trading cost to 7 cents as discussed
+                 trading_cost: float=0.07,
+                 starting_cash: float=100_000.0,
+                 q_trading_thres: float=0.2,
+                 fx_look_ahead: int=1,
+                 # maybe include max assets=5?
+                 leverage_multiplier: float=5.0):
+        
+        # load 
+        self.train_pred = train_predictions
+        self.test_pred = test_predictions
+        self.entry_thresh = entry_thresholds
+        self.exit_thresh = exit_thresholds
         self.trading_cost = trading_cost
         self.q_trading_thres = q_trading_thres
         self.fx_look_ahead = fx_look_ahead
@@ -25,11 +40,20 @@ class Backtester:
     def run_backtest(self):
         if self.backtest_ran:
             raise Exception("Backtest already run")
+        
+        # initialize logging because it's certainly going to crash
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            filename='app.log', # Saves to a file instead of the console
+            
+            # we want to append values
+            filemode='a'        
+        )
         self.backtest_ran = True
 
         # Iterate through each entry in signals and execute trades as needed
-        for t in self.signals_df.index:
-            pass
+        
 
 
     def get_backtest_results(self):
--- backtester/main.py
+++ backtester/main.py
@@ -1,14 +1,21 @@
 from backtester_engine import Backtester
 from performance_output import plot_portfolio_history, print_portfolio_stats
-from data import read_signal_data
+import pandas as pd
 
 
 def main():
     # Get signals data
-    signals_df = read_signal_data()
+    train_predictions = pd.read_csv("../data/rf_train_predictions.csv")
+    test_predictions = pd.read_csv("../data/rf_test_predictions.csv")
+    entry_thresholds = pd.read_csv("../data/rf_thresholds.csv")
+    exit_thresholds = pd.read_csv("../data/rf_exit_thresholds")
+    
 
     # Run backtesting simulation given parameters
-    backtester = Backtester(signals_df=signals_df)
+    backtester = Backtester(train_predictions=train_predictions, 
+                            test_predictions=test_predictions,
+                            entry_thresholds=entry_thresholds,
+                            exit_thresholds=exit_thresholds)
     backtester.run_backtest()
 
     # Display results
